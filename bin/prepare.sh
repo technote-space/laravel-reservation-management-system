0a3631cc791dad7f693e1627fab5dccf2c7779c6
Technote <technote.space@gmail.com>
2019-07-27 03:39:36 +0900

    fix: ci

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -6,23 +6,22 @@ current=$(cd $(dirname $0);
 pwd)
 source ${current}/variables.sh
 
-if [[ "${TRAVIS_BUILD_STAGE_NAME}" == "Test" ]]; then
+if [[ "${TRAVIS_BUILD_STAGE_NAME}" == "Deploy" ]]; then
+    echo ""
+    echo ">> Build"
+    composer build
+else
     echo ""
     echo ">> Setup"
     composer setup
+fi
 
+if [[ "${TRAVIS_BUILD_STAGE_NAME}" == "Test" ]]; then
     echo ""
     echo ">> Setup Database"
     psql -c 'create database travis_ci_test;' -U postgres
     cp .env.travis .env
     php artisan key:generate
-fi
-
-if [[ "${TRAVIS_BUILD_STAGE_NAME}" == "Deploy" ]]; then
-    echo ""
-    echo ">> Build"
-
-    composer build
 fi
 
 if [[ -n "${LARAVEL_DUSK}" ]]; then
