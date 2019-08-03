cd4a6770f13f3c8b56d74ca6faabc504d5f6a4e4
Technote <technote.space@gmail.com>
2019-08-04 01:45:56 +0900

    chore: setup moment

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,15 @@
 require('./bootstrap');
 require('./plugins/validate');
 require('./plugins/toasted');
+require('moment-timezone');
 import 'babel-polyfill';
 import Vue from 'vue';
 import router from './router';
 import store from './store';
 import App from './components/App';
+import moment from 'moment';
+
+moment.tz.setDefault('Asia/Tokyo');
 
 import vuetify from './plugins/vuetify';
 
