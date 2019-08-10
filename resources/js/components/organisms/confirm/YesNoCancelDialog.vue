8a0fd1b721f9d484ca98c2d807dac9e0aea89a87
Technote <technote.space@gmail.com>
2019-08-11 04:12:42 +0900

    feat: confirm dialog

---
+++ resources/js/components/organisms/confirm/YesNoCancelDialog.vue
@@ -0,0 +1,74 @@
+<template>
+    <v-dialog
+        :value="dialog"
+        @click:outside="cancel"
+        max-width="400px"
+    >
+        <v-card>
+            <v-card-title>
+                <span class="headline">{{ $t(title) }}</span>
+            </v-card-title>
+
+            <v-card-text>
+                {{ $t(message) }}
+            </v-card-text>
+
+            <v-card-actions class="d-block text-center">
+                <v-btn
+                    color="primary"
+                    dark
+                    class="mb-2"
+                    @click.stop="yes"
+                >
+                    {{ $t('misc.yes') }}
+                </v-btn>
+                <v-btn
+                    color="primary"
+                    dark
+                    class="mb-2"
+                    @click.stop="no"
+                >
+                    {{ $t('misc.no') }}
+                </v-btn>
+                <v-btn
+                    color="primary"
+                    dark
+                    class="mb-2"
+                    @click.stop="cancel"
+                >
+                    {{ $t('misc.cancel') }}
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+    export default {
+        props: {
+            dialog: {
+                type: Boolean,
+                required: true,
+            },
+            title: {
+                type: String,
+                default: 'misc.confirm',
+            },
+            message: {
+                type: String,
+                required: true,
+            },
+        },
+        methods: {
+            yes () {
+                this.$emit('yes');
+            },
+            no () {
+                this.$emit('no');
+            },
+            cancel () {
+                this.$emit('cancel');
+            },
+        },
+    };
+</script>
