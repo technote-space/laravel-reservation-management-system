d971d660eaa68ea86b3d27e934a6db1af4d04e1d
Technote <technote.space@gmail.com>
2019-08-12 01:08:59 +0900

    fix: mock

--- resources/js/store/modules/mock/mutations.js
+++ resources/js/store/modules/mock/mutations.js
@@ -1,22 +1,35 @@
+import { camelCase } from 'lodash';
 import { CREATE, UPDATE, DELETE, LOGIN, LOGOUT } from './constant';
 
 const mutations = {
     [ CREATE ] (state, { model, data }) {
-        state.items[ model ] = Object.assign({}, state.items[ model ]);
-        const item = state.factories[ model ]().create(data[ model ]);
+        state.items = Object.assign({}, state.items);
+
+        const camel = camelCase(model);
+        const item = state.factories[ camel ]().create(data[ model ]);
         state.items[ model ][ item.id ] = item;
+        const foreignKey = model.replace(/e?s$/, '') + '_id';
+        const foreignId = item.id;
+
+        delete data[ model ];
+        Object.keys(data).map(model => {
+            const camel = camelCase(model);
+            data[ model ][ foreignKey ] = foreignId;
+            const item = state.factories[ camel ]().create(data[ model ]);
+            state.items[ camel ][ item.id ] = item;
+        });
     },
-    [ UPDATE ] (state, { model, id, data }) {
-        if (id in state.items[ model ]) {
-            state.items[ model ] = Object.assign({}, state.items[ model ]);
-            state.items[ model ][ id ] = Object.assign({}, state.items[ model ][ id ], data[ model ]);
-        }
+    [ UPDATE ] (state, { id, data }) {
+        state.items = Object.assign({}, state.items);
+        Object.keys(data).map(model => {
+            const camel = camelCase(model);
+            state.items[ camel ][ id ] = Object.assign({}, state.items[ camel ][ id ], data[ model ]);
+        });
     },
     [ DELETE ] (state, { model, id }) {
-        if (id in state.items[ model ]) {
-            state.items[ model ] = Object.assign({}, state.items[ model ]);
-            delete state.items[ model ][ id ];
-        }
+        const camel = camelCase(model);
+        state.items[ camel ] = Object.assign({}, state.items[ camel ]);
+        delete state.items[ camel ][ id ];
     },
     [ LOGIN ] (state, { user }) {
         state.user = user;
