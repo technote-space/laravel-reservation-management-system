7ebb939bc462869aa0f1e71166a1ab1a83805f73
Technote <technote.space@gmail.com>
2019-08-14 02:50:44 +0900

    feat: add test

--- tests/Unit/Helpers/FileHelperTest.php
+++ tests/Unit/Helpers/FileHelperTest.php
@@ -43,4 +43,17 @@ class FileHelperTest extends TestCase
         $this->assertStringStartsWith('http', self::$helper->getStorageUrl('https://localhost'));
         $this->assertStringStartsWith('http', self::$helper->getStorageUrl('test', 'public'));
     }
+
+    public function testLoadJson()
+    {
+        $json = self::$helper->loadJson(resource_path('seed/env.json'))->toArray();
+        $this->assertTrue(is_array($json));
+        $this->assertNotEmpty($json);
+        $this->assertArrayHasKey('prod', $json);
+        $this->assertArrayHasKey('dev', $json);
+
+        $json = self::$helper->loadJson(resource_path('seed/env.json.test'))->toArray();
+        $this->assertTrue(is_array($json));
+        $this->assertEmpty($json);
+    }
 }
