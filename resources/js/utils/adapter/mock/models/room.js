94cf4714d5fabc47a41fe26f0d2604905510f8fd
Technote <technote.space@gmail.com>
2019-08-22 02:51:45 +0900

    chore: remove unused data

--- resources/js/utils/adapter/mock/models/room.js
+++ resources/js/utils/adapter/mock/models/room.js
@@ -1,4 +1,3 @@
-import { orderBy, first, filter, take } from 'lodash';
 import { orderBy, sumBy } from 'lodash';
 import store from '../../../../store';
 import reservation from './reservation';
@@ -7,9 +6,6 @@ export default item => {
     const reservations = orderBy(store.getters[ 'adapter/searchAll' ]('reservations', reservation => reservation.room_id === item.id), ['start_date'], 'desc');
     return Object.assign({}, item, {
         is_reserved: false,
-        latest_reservation: first(reservations),
-        latest_usage: first(filter(reservations, reservation => !moment(reservation.start_date).isAfter())),
-        recent_usages: take(filter(reservations, reservation => !moment(reservation.start_date).isAfter()), 5),
         total_sales: sumBy(reservations, function (item) {
             return reservation(item, false).detail.payment - 0;
         }),
