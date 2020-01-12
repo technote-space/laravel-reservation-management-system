94b7bf342515c0cce7993028d5332bf9528e91f4
Technote <technote.space@gmail.com>
2020-01-12 23:10:33 +0900

    feat: add components tests

---
+++ resources/js/__tests__/components/organisms/Sales.spec.js
@@ -0,0 +1,43 @@
+import { mount } from '@vue/test-utils';
+import Vuex from 'vuex';
+import Sales from '../../../components/organisms/Sales';
+import app from '../app';
+import setupLocalVue from '../setupLocalVue';
+
+jest.useFakeTimers();
+jest.mock('../../../utils/api', () => ({
+    apiGet: () => ({}),
+}));
+jest.mock('vue-chartjs', () => ({
+    Bar: {
+        render: () => undefined,
+        methods: {
+            renderChart: () => undefined,
+        },
+    },
+}));
+
+describe('Sales', () => {
+    it('should render sales', () => {
+        const wrapper = mount(app(Sales, 'inner', {
+            props: {
+                label: 'test',
+                start: '2020-01-01',
+                end: '2020-01-31',
+                type: 'test',
+                roomId: 1,
+            },
+        }), setupLocalVue({
+            store: new Vuex.Store({
+                state: {
+                    data: [],
+                    isLoading: false,
+                },
+            }),
+        }));
+        expect(wrapper.element).toMatchSnapshot();
+
+        expect(wrapper.isVueInstance()).toBeTruthy();
+        expect(wrapper.findAll('circle')).toHaveLength(1);
+    });
+});
