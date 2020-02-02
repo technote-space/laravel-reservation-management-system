e5ba2e74988566cd6704aee17a1c16cecf77526d
Technote <technote.space@gmail.com>
2020-02-02 23:50:15 +0900

    fix: code formattings

--- resources/js/store/modules/adapter/mock/factories/guest-detail.js
+++ resources/js/store/modules/adapter/mock/factories/guest-detail.js
@@ -2,10 +2,10 @@ import faker from '../faker';
 import base from './base';
 
 export default base('guest-detail', () => ({
-    guest_id: null,
+    'guest_id': null,
     name: faker.name.firstName() + ' ' + faker.name.lastName(),
-    name_kana: 'カナ',
-    zip_code: faker.address.zipCode(),
+    'name_kana': 'カナ',
+    'zip_code': faker.address.zipCode(),
     address: faker.address.city(),
     phone: faker.phone.phoneNumber(),
 }));
